20d463bd04fe97c0f6625de478ae6508c65f0b9f
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2020-02-02 14:11:48 +0100

    Update voicieh.cpp

--- voicieh.cpp
+++ voicieh.cpp
@@ -2,13 +2,12 @@
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
-using namespace std;
 
 void pause_n_clear()
 {
 #ifdef _WIN32
-    system("ECHO Aby przejsc dalej nacisnij dowolny klawisz . . .");
-    system("ECHO Przyciski typu zasilanie nie wchodza w gre,hehe . . .");
+    system("echo Aby przejsc dalej nacisnij dowolny klawisz . . .");
+    system("echo Przyciski typu zasilanie nie wchodza w gre,hehe . . .");
     system("PAUSE >NUL");
     system("CLS");
 #elif __unix__
@@ -17,16 +16,15 @@ void pause_n_clear()
     system("read -p ... .");
     system("clear");
 #elif __APPLE__
-    cout << "JABKO..." << endl;
+    std::cout << "JABKO..." << std::endl;
     system("echo Aby przejsc dalej nacisnij dowolny klawisz . . .");
     system("echo Przyciski typu zasilanie nie wchodza w gre,hehe . . .");
     system("read -p ... .");
     system("clear");
 #else
-    cout << "OS not supported!" << endl;
+    std::cout << "OS not supported!" << std::endl;
 #endif
 }
-
 void clear_window()
 {
 #ifdef _WIN32
@@ -34,80 +32,111 @@ void clear_window()
 #elif __unix__
     system("clear");
 #elif __APPLE__
-    cout << "APPLE..." << endl;
+    std::cout << "APPLE..." << std::endl;
     system("clear");
 #else
-    cout << "OS not supported!" << endl;
+    std::cout << "OS not supported!" << std::endl;
 #endif
 }
-
-void open_file(string &filename)
+void open_file(std::string &filename)
 {
 #ifdef _WIN32
     filename = '"' + filename;
     filename += '"';
-    string win = "TYPE " + filename;
-    cout << win << endl;
+    std::string win = "TYPE " + filename;
+    std::cout << win << std::endl;
     char* temp = new char [win.length()+1];
     for(int i = 0; i < win.length(); i++)
         temp[i] = win[i];
     temp[win.length()] = 0;
     system(temp);
 #elif __unix__
-    string linux = "cat " + filename;
+    std::string linux = "cat " + filename;
     char* temp = new char [linux.length()];
     for(int i = 0; i < linux.length(); i++)
         temp[i] = linux[i];
     temp[linux.length()] = 0;
     system(temp);
 #elif __APPLE__
-    cout << "APPLE..." << endl;
-    string apple = "cat" + filename;
+    std::cout << "APPLE..." << std::endl;
+    std::string apple = "cat" + filename;
     char* temp = new char [apple.length()];
     for(int i = 0; i < linux.length(); i++)
         temp[i] = apple[i];
     temp[apple.length()] = 0;
     system(temp);
 #else
-    cout << "OS not supported!" << endl;
+    std::cout << "OS not supported!" << std::endl;
 #endif
 }
 
-void convert_to_lower_case(string &s)
+void convert_to_lower_case(std::string &s)
 {
     for(int i = 0; i < s.length(); i++)
         s[i] = tolower(s[i] );
 }
-
-void print_opening()
+std::string filename()
 {
-    cout << " VV         VV OOOO IIIII" << endl
-         << "  VV       VV O    O  I" << endl
-         << "   V       V  O    O  I" << endl
-         << "    V     V   O    O  I" << endl
-         << "     V   V    O    O  I" << endl
-         << "      V V     O    O  I" << endl
-         << "       V       OOOO IIIII" << endl
-         << "Copyright (C) 2019 - 2020 Wojcieh Bia³ek" << endl
-         << "This program comes with ABSOLUTELY NO WARRANTY." << endl
-         << "This is free software, and you are welcome to redistribute it." << endl
-         << "under certain conditions for details read LICENSE." << endl;
+    std::string filename;
+    std::cout << "Podaj nazwe pliku:";
+    std::cin >> filename;
+    filename += ".voi";
+    return filename;
 }
 
-void print_help(string* keywords,string* description)
+void nadpisz()
 {
-    cout << "Lista slovek kluczowych:" << endl;
+    std::string s,fname = filename();
+    std::ofstream file;
+    file.open(fname);
+    file << "Plik zostal utworzony za pomocom jenzyka VOICIEH."<< "\n"<< "-------------------------------------------------";
+    std::cout << "Podaj wyraz do dopisania:";
+    std::cin >> s;
+    file << "\n" << s ;
+    file.close();
+}
+
+void dopisz()
+{
+    std::string s,fname = filename();
+    std::ofstream file;
+    file.open(fname,std::ios::app);
+    std::cout << "Podaj wyraz nadpisujoncy:";
+    std::cin >> s;
+    file << "\n" << s ;
+    file.close();
+}
+
+void powiedz()
+{
+    std::string s;
+    std::cin >> s;
+    std::cout << "Witaj kmiocie!" << std::endl
+              << s << "?" << std::endl;
+}
+
+void otworz()
+{
+    std::string fname = filename();
+    open_file(fname);
+    std::cout << std::endl
+              << "KONIEC PLIKU" << std::endl;
+}
+
+void print_help(std::string* keywords,std::string* description)
+{
+    std::cout << "Lista slovek kluczowych:" << std::endl;
     for(int i=0; i<22; i++)
-        cout << "-";
-    cout << endl;
+        std::cout << "-";
+    std::cout << std::endl;
     for (int i=0; i<15; i++)
-        cout << "<" << keywords[i] << ">" << "::=" << description[i] << endl;
+        std::cout << "<" << keywords[i] << ">" << "::=" << description[i] << std::endl;
 }
 
-void get_from_file_to_array(string filename,string* array_a,int s_i_z_e,char delimiter)
+void get_from_file_to_array(std::string filename,std::string* array_a,int s_i_z_e,char delimiter)
 {
-    string s;
-    ifstream fptr (filename);
+    std::string s;
+    std::ifstream fptr (filename);
     for(int i = 0; i < s_i_z_e; i++)
     {
         getline(fptr,s,delimiter);
@@ -118,54 +147,53 @@ void get_from_file_to_array(string filename,string* array_a,int s_i_z_e,char del
 
 void show_interpreter_in_console()
 {
-    print_opening();
-
+    std::string opening = " VV         VV OOOO IIIII\n  VV       VV O    O  I\n   V       V  O    O  I\n    V     V   O    O  I\n     V   V    O    O  I\n      V V     O    O  I\n       V       OOOO IIIII\nCopyright (C) 2019 - 2020 Wojcieh Bia³ek\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it.\nunder certain conditions for details read LICENSE.\n";
+    std::cout << opening;
     pause_n_clear();
 
     int a=0;
-    string input;
-    string* keywords = new string[17];
-    string* descriptions = new string[15];
-
+    std::string input;
+    std::string* keywords = new std::string[17];
+    std::string* descriptions = new std::string[15];
     get_from_file_to_array("keywords.voi",keywords,17,' ');
     get_from_file_to_array("descriptions.voi",descriptions,15,',');
 
     while(3!=5)
     {
-        cout << "VOI" << endl;
-        cin >> input;
+        std::cout << "VOI" << std::endl;
+        std::cin >> input;
         convert_to_lower_case(input);
 
         if( input == keywords[0] )
-            cout << "Witaj kmiocie!" << endl;
+            std::cout << "Witaj kmiocie!" << std::endl;
         else if( input == keywords[1] )
         {
-            cout << "Podaj zawartosc pojemnika 'a'." << endl
-                 << "a:";
-            cin >> a;
+            std::cout << "Podaj zawartosc pojemnika 'a'." << std::endl
+                      << "a:";
+            std::cin >> a;
         }
         else if( input == keywords[2] )
-            cout << "Zawartosc pojemnika 'a':" << a << endl;
+            std::cout << "Zawartosc pojemnika 'a':" << a << std::endl;
         else if( input == keywords[3] || input == keywords[5] )
         {
             int bucket = 0;
-            cout << "Podaj wartosc,ktora zostanie dodana do pojemnika 'a':" << endl;
-            cin >> bucket;
+            std::cout << "Podaj wartosc,ktora zostanie dodana do pojemnika 'a':" << std::endl;
+            std::cin >> bucket;
             a+=bucket;
             bucket=0;
         }
         else if( input == keywords[4] || input == keywords[6] )
         {
             int bucket = 0;
-            cout << "Podaj wartosc,ktora zostanie odjeta od pojemnika 'a':" << endl;
-            cin >> bucket;
+            std::cout << "Podaj wartosc,ktora zostanie odjeta od pojemnika 'a':" << std::endl;
+            std::cin >> bucket;
             a-=bucket;
         }
         else if( input == keywords[7] )
         {
             int bucket = 0;
-            cout << "Podaj wartosc,przez ktora zostanie pomnozona zawartosc pojemnika 'a':" << endl;
-            cin >> bucket;
+            std::cout << "Podaj wartosc,przez ktora zostanie pomnozona zawartosc pojemnika 'a':" << std::endl;
+            std::cin >> bucket;
             a*=bucket;
         }
         else if( input == keywords[8] )
@@ -173,8 +201,8 @@ void show_interpreter_in_console()
             int bucket;
             while(bucket == 0)
             {
-                cout << "Podaj wartosc,przez ktora zostanie podzielona zawartosc pojemnika 'a':" << endl;
-                cin >> bucket;
+                std::cout << "Podaj wartosc,przez ktora zostanie podzielona zawartosc pojemnika 'a':" << std::endl;
+                std::cin >> bucket;
             }
             a/=bucket;
         }
@@ -183,53 +211,17 @@ void show_interpreter_in_console()
         else if( input == keywords[10] )
             return;
         else if( input == keywords[11] )
-        {
-            string s,filename;
-            ofstream file;
-            cout << "Podaj nazwe pliku:";
-            cin >> filename;
-            filename += ".voi";
-            file.open(filename);
-            file << "Plik zostal utworzony za pomocom jenzyka VOICIEH."<< "\n"<< "-------------------------------------------------";
-            cout << "Podaj wyraz do dopisania:";
-            cin >> s;
-            file << "\n" << s ;
-            file.close();
-        }
+            nadpisz();
         else if( input == keywords[12] )
-        {
-            string s,filename;
-            ofstream file;
-            cout << "Podaj nazwe pliku:";
-            cin >> filename;
-            filename += ".voi";
-            file.open(filename,ios::app);
-            cout << "Podaj wyraz nadpisujoncy:";
-            cin >> s;
-            file << "\n" << s ;
-            file.close();
-        }
+            dopisz();
         else if( input == keywords[13] )
-        {
-            string s;
-            cin >> s;
-            cout << "Witaj kmiocie!" << endl
-                 << s << "?" << endl;
-        }
+            powiedz();
         else if( input == keywords[14] )
-        {
-            string filename;
-            cout << "Podaj nazwe pliku:";
-            cin >> filename;
-            filename += ".voi";
-            open_file(filename);
-            cout << endl
-                 << "KONIEC PLIKU" << endl;
-        }
+            otworz();
         else if( input == keywords[15] || input == keywords[16] )
             print_help(keywords,descriptions);
         else
-            cout << "Brak takiego slovka w slovnika" << endl;
+            std::cout << "Brak takiego slovka w slovnika" << std::endl;
     }
 }
 
